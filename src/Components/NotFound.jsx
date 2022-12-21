0ba76db8dee81b7752ffbb860920a1254b738b99
Domains18 <gibsonsgibson88@gmail.com>
2022-12-21 16:04:11 +0300

    'react hooks

---
+++ src/Components/NotFound.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { poster } from './Poster';
+import { Rohe } from './Rohe';
+
+
+
+
+
+export function NotFound() {
+    return 
+    <Rohe>
+        <poster title="404" subtitle="Page not found">
+            <span className="btn-btn-home">Click on the hooks on navabr</span>
+        </poster>
+    </Rohe>
+}
